b3b312522614e9ce06c9a5bbb33a3d41d2cba4d0
Mike Kühnapfel <veyxos@gmail.com>
2019-10-08 21:34:59 +0200

    add calendar and protocol post type

--- functions.php
+++ functions.php
@@ -16,3 +16,42 @@ function pp_init() {
     ));
 }
 add_action('after_setup_theme', 'pp_init');
+
+// ADD CALENDAR POST TYPE
+function pp_calender_post_type() {
+    register_post_type('calendar', array(
+        'rewrite' => array( 'slug' => 'termine' ),
+        'labels' => array(
+            'name' => 'Termine',
+            'singular_name' => 'Termin',
+            'add_new_item' => 'Neuen Termin hinzufügen',
+            'edit_item' => 'Termin bearbeiten'
+        ),
+        'menu_icon' => 'dashicons-calendar-alt',
+        'public' => true,
+        'has_archive' => true,
+        'supports' => array( 'title', 'thumbnail', 'editor', 'custom-fields' )
+    ));
+    flush_rewrite_rules(false);
+}
+add_action('init', 'pp_calender_post_type');
+flush_rewrite_rules(false);
+
+// // ADD PROTOCOL POST TYPE
+function pp_protocol_post_type() {
+    register_post_type('protocol', array(
+        'rewrite' => array( 'slug' => 'protokolle' ),
+        'labels' => array(
+            'name' => 'Protokolle',
+            'singular_name' => 'Protokoll',
+            'add_new_item' => 'Protokoll anlegen',
+            'edit_item' => 'Protokoll überarbeiten'
+        ),
+        'menu_icon' => 'dashicons-clipboard',
+        'public' => true,
+        'has_archive' => true,
+        'supports' => array( 'title', 'editor', 'excerpt', 'custom-fields', 'revision' )
+    ));
+    flush_rewrite_rules(false);
+}
+add_action('init', 'pp_protocol_post_type');
