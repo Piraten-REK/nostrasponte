60aa8936f54f60404a3183d56e6b7efa9541a6da
Mike Kühnapfel <veyxos@gmail.com>
2019-10-17 12:19:03 +0200

    add js to make dropdown menus in navbar stayopen on click

--- js/main.js
+++ js/main.js
@@ -21,7 +21,33 @@ function initShareButtons() {
     })
 }
 
+function initNavBarButtons() {
+    document.querySelectorAll("#site_nav > ul > li").forEach(it => {
+        /** @type Array<Element> */
+        const children = [...it.children];
+        /** @type boolean */
+        const dropdown = children.filter(e => e.tagName.match(/button/i)).length > 0;
+        /** @type boolean */
+        const open = children.filter(e => e.tagName.match(/button/i)).filter(e => e.parentElement.classList.contains("open")).length > 0;
+        const closeAllOthers = () => {
+            [...document.querySelectorAll("#site_nav > ul > li.open")].filter(e => e !== it).forEach(e => e.classList.remove("open"));
+        }
+        it.addEventListener("mouseover", closeAllOthers);
+        if (dropdown) {
+            children.filter(e => e.tagName.match(/button/i))[0].addEventListener("click", () => {
+                if (open) {
+                    it.classList.remove("open");
+                } else {
+                    closeAllOthers();
+                    it.classList.add("open");
+                }
+            });
+        }
+    });
+}
+
 ready(() => {
     console.log(hello())
     initShareButtons()
+    initNavBarButtons()
 })
