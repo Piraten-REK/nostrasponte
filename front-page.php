5ccfc564dc70ccc99803559ce0e61eef226b1f6f
Mike Kühnapfel <veyxos@gmail.com>
2019-10-31 17:21:18 +0100

    change span to time tags for date in front-page and change the renderng of dates

--- front-page.php
+++ front-page.php
@@ -2,11 +2,25 @@
 $args = array(
     'posts_per_page' => 1
 );
+$months = array(
+    '1' => 'Januar',
+    '2' => 'Februar',
+    '3' => 'März',
+    '4' => 'April',
+    '5' => 'Mai',
+    '6' => 'Juni',
+    '7' => 'Juli',
+    '8' => 'August',
+    '9' => 'September',
+    '10' => 'Oktober',
+    '11' => 'November',
+    '12' => 'Dezember'
+);
 
 $hero = new WP_Query($args);
 while ($hero -> have_posts()): $hero -> the_post(); ?>
 <section id="hero" style="background-image: url('<?php echo has_post_thumbnail(get_the_ID()) ? get_the_post_thumbnail_url(get_the_ID()) : get_theme_file_uri('img/standard_hero.jpg'); ?>">
-    <p class="metadata"><span><span class="category"><?php the_category(' &bull; '); ?></span> <span class="date"><?php the_date('d. M Y'); ?></span></span></p>
+    <p class="metadata"><span><span class="category"><?php the_category(' &bull; '); ?></span> <time datetime="<?php the_date('Y-m-d'); ?>" class="date"><?php echo get_the_date('d') . '. ' . $months[get_the_date('n')] . ' ' . get_the_date('Y'); ?></time></span></p>
     <header>
         <h2><?php the_title(); ?></h2>
     </header>
@@ -28,7 +42,7 @@ while ($hero -> have_posts()): $hero -> the_post(); ?>
                 <header>
                     <p class="metadata">
                         <span class="category"><?php the_category(' &bull; '); ?></span>
-                        <span class="date"><?php the_date('d. M Y') ?></span>
+                        <time datetime="<?php the_date('Y-m-d'); ?>" class="date"><?php echo get_the_date('d') . '. ' . $months[get_the_date('n')] . ' ' . get_the_date('Y'); ?></time>
                     </p>
                     <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                 </header>
