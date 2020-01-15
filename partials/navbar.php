d9cfeaa8a124400d167d5b06ee47cf8b1d0de047
Mike Kühnapfel <veyxos@gmail.com>
2020-01-15 22:12:02 +0100

    change navbar temporarily

--- partials/navbar.php
+++ partials/navbar.php
@@ -15,17 +15,19 @@ function checkPages($arr) {
     <ul>
         <li <?php setActive(is_front_page()); ?>> <!-- Home -->
             <a href="<?php echo is_front_page() ? '#' : site_url(''); ?>">Home</a>
-        </li><li <?php setActive(get_post_type() === 'post' || is_post_type_archive('post')); ?>> <!-- Aktuelles -->
+        </li><li <?php setActive(get_post_type() === 'post' || is_post_type_archive('post') || checkPages(array('kreisverband', 'bruehl', 'erftstadt', 'frechen', 'kerpen'))); ?>> <!-- Aktuelles -->
             <button>Vor Ort</button>
             <ul>
+                <li><a href="<?php echo site_url('kreisverband'); ?>" <?php setActive(is_page('kreisverband')); ?>>Kreisverband</a></li>
+                <hr>
                 <!-- <li><a href="<?php echo site_url('stadt/bedburg'); ?>" <?php setActive(is_category('bedburg')); ?>>Bedburg</a></li> -->
                 <!-- <li><a href="<?php echo site_url('stadt/bergheim'); ?>" <?php setActive(is_category('bergheim')); ?>>Bergheim</a></li> -->
-                <li><a href="<?php echo site_url('stadt/bruehl'); ?>" <?php setActive(is_category('bruehl')); ?>>Brühl</a></li>
+                <li><a href="<?php echo site_url('bruehl'); ?>" <?php setActive(is_page('bruehl')); ?>>Brühl</a></li>
                 <!-- <li><a href="<?php echo site_url('stadt/elsdorf'); ?>" <?php setActive(is_category('elsdorf')); ?>>Elsdorf</a></li> -->
-                <li><a href="<?php echo site_url('stadt/erftstadt'); ?>" <?php setActive(is_category('erftstadt')); ?>>Erftstadt</a></li>
-                <li><a href="<?php echo site_url('stadt/frechen'); ?>" <?php setActive(is_category('frechen')); ?>>Frechen</a></li>
+                <li><a href="<?php echo site_url('erftstadt'); ?>" <?php setActive(is_page('erftstadt')); ?>>Erftstadt</a></li>
+                <li><a href="<?php echo site_url('frechen'); ?>" <?php setActive(is_page('frechen')); ?>>Frechen</a></li>
                 <!-- <li><a href="<?php echo site_url('stadt/huerth'); ?>" <?php setActive(is_category('huerth')); ?>>Hürth</a></li> -->
-                <li><a href="<?php echo site_url('stadt/kerpen'); ?>" <?php setActive(is_category('kerpen')); ?>>Kerpen</a></li>
+                <li><a href="<?php echo site_url('kerpen'); ?>" <?php setActive(is_page('kerpen')); ?>>Kerpen</a></li>
                 <!-- <li><a href="<?php echo site_url('stadt/pulheim'); ?>" <?php setActive(is_category('pulheim')); ?>>Pulheim</a></li> -->
                 <!-- <li><a href="<?php echo site_url('stadt/wesseling'); ?>" <?php setActive(is_category('wesseling')); ?>>Wesseling</a></li> -->
             </ul>
