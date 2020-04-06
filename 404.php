b81cc1ab937c13a60f1f0a24146f372a208975b3
Mike Kühnapfel <mike.kuehnapfel@smail.th-koeln.de>
2020-04-06 23:32:25 +0200

    change 404 page

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
     <h2 title="Error 404 - Not Found">404</h2>
 </header>
 <main>
-    <p>Deine angeforderte Seie <span class="pre"><?php echo esc_attr(urldecode($_SERVER['REQUEST_URI'])); ?></span> konnte nicht gefunden werden.</p>
+    <p>Die Seite <span class="pre"><?php echo esc_attr(urldecode($_SERVER['REQUEST_URI'])); ?></span> kann nicht gefunden werden, außer von denen die wissen wo sie ist.</p>
     <a href="javascript:window.history.back()" class="btn no_img" id="btn_back">Zurück</a>
 </main>
 
