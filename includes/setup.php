72f925c1ecaf59d72fd8caefe8f9c5fd595dd730
Mike Kühnapfel <mike.kuehnapfel@smail.th-koeln.de>
2020-04-08 23:02:40 +0200

    activate complete theme support

--- includes/setup.php
+++ includes/setup.php
@@ -4,5 +4,9 @@ function ns_setup_theme() {
 	add_theme_support('post-thumbnails');
 	add_theme_support('automatic-feed-links');
 	add_theme_support('title-tag');
-	add_theme_support('html5', ['search-form', 'gallery', 'caption']);
+	add_theme_support('html5');
+
+	register_nav_menu('primary', 'Primäres Menü');
+	register_nav_menu('footer_social', 'Social Media');
+	register_nav_menu('footer_links', 'Footer Links');
 }
