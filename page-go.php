ad57e3972a3cef5e9747738222da0ef0123a83ce
Mike Kühnapfel <mike.kuehnapfel@smail.th-koeln.de>
2020-04-08 22:40:36 +0200

    exclude content for wiki pages to a template part

--- page-go.php
+++ page-go.php
@@ -1,11 +1,5 @@
-<?php get_header(); ?>
-<script defer="defer" src="<?php echo get_theme_file_uri('/js/wiki.min.js'); ?>"></script>
-<noscript>
-    <main>
-        <h2>Geschäftsordnung</h2>
-        <a href="https://wiki.piratenpartei.de/NRW:Rhein-Erft-Kreis/Kreisverband/Satzung" class="btn external-link-img-wrapper" rel="external">Zur Satzung</a>
-    </main>
-</noscript>
-
-<?php include('partials/sidebar.php');
-get_footer(); ?>
+<?php
+get_header();
+get_template_part('partials/wiki-page');
+get_sidebar();
+get_footer();
---
+++ partials/wiki-page.php
@@ -0,0 +1,7 @@
+<script defer="defer" src="<?php echo get_theme_file_uri('/assets/js/wiki.min.js'); ?>"></script>
+<noscript>
+	<main>
+		<h2><?php the_title(); ?>></h2>
+		<a href="https://wiki.piratenpartei.de/NRW:Rhein-Erft-Kreis/Kreisverband/Satzung" class="btn external-link-img-wrapper" rel="external">Zur Satzung</a>
+	</main>
+</noscript>
