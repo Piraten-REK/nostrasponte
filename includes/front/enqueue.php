dec366884f3f571ddcc9e68e18820a442dd3bc55
Mike Kühnapfel <mike.kuehnapfel@smail.th-koeln.de>
2020-04-09 23:08:36 +0200

    tidy up script and style enqueue script

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -2,19 +2,17 @@
 
 function ns_enqueue() {
 	$uri = get_theme_file_uri();
-	$ver = NOSTRASPONTE_DEV_MODE ? time() : false;
+	function ver(string $path) {
+		return NOSTRASPONTE_DEV_MODE ? filemtime(get_theme_file_path($path)) : false;
+	}
 
-	wp_register_style('main-css', $uri . '/assets/css/main.css', [], $ver);
+	wp_register_style('main-css', $uri . '/assets/css/main.css', [], ver('/assets/css/main.css'));
 
 	wp_enqueue_style('main-css');
 
-	wp_register_script('main-js', $uri . '/assets/js/main.min.js', [], $ver, true);
-	wp_register_script('tetris', $uri . '/assets/js/tetris.min.js', [], $ver, true);
+	wp_register_script('main-js', $uri . '/assets/js/main.min.js', [], ver('/assets/js/main.min.js'), true);
+	wp_register_script('tetris-js', $uri . '/assets/js/tetris.min.js', [], ver('/assets/js/tetris.min.js'), true);
 
 	wp_enqueue_script('main-js');
 	wp_enqueue_script('tetris-js');
-
-//	wp_enqueue_style('main', get_stylesheet_uri(), null, $ver , 'all');
-//	wp_enqueue_script('main-js', get_theme_file_uri('js/main.min.js'), NULL, microtime(), true);
-//	wp_enqueue_script('tetris-js', get_theme_file_uri('js/tetris.min.js'), NULL, microtime(), true);
 }
