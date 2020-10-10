5251de61f88ca9ab7aa8aed5b7a2f24a0e904999
Mike Kühnapfel <mike.kuehnapfel@smail.th-koeln.de>
2020-10-10 16:16:41 +0200

    add class `index-php` to `<body>` tag if using `index.php` as template

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=dege">
 	<?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
+<body <?php body_class( is_index() ? 'index-php' : '' ); ?>>
 <header class="site-header px-2 py-1 px-md-4 px-lg-6 container">
 	<a href="<?php echo is_front_page() ? '#' : get_home_url(); ?>" class="site-header__logo"><img class="site-header__logo__img" src="<?php echo get_theme_file_uri('/assets/img/logo.svg'); ?>" alt="Piraten Rhein-Erft-Kreis"></a>
 	<button class="site-header__menu-button site-header__menu-button--closed" id="nav__toggle">
