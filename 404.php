43070cdd6b508065c5b079336385827b4af3281e
Mike Kühnapfel <mike.kuehnapfel@smail.th-koeln.de>
2020-10-14 16:21:32 +0200

    add `ml-minus` class to back buttons
    added `ml-minus` class to the <i>-tags in the back buttons in the 404
    and index templates

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@ get_header();
 		<section class="error-content px-2 py-8 px-md-4 py-15 px-lg-6">
 			<h1 class="error-content__title mb-3 mb-lg-4" lang="en-US">Your princess is in another castle</h1>
 			<p class="error-content__text mb-6 mb-lg-12"><?php esc_html_e( 'Die von Dir gesuchte Seite konnte nicht gefunden werden.', 'nostrasponte' ); ?></p>
-			<p class="center"><a href="javascript:window.history.back()" class="btn center"><i class="feather icon-arrow-left"></i> <?php esc_html_e( 'Zurück', 'nostrasponte' ); ?></a></p>
+			<p class="center"><a href="javascript:window.history.back()" class="btn center"><i class="feather icon-arrow-left ml-minus"></i> <?php esc_html_e( 'Zurück', 'nostrasponte' ); ?></a></p>
 		</section>
 	</main>
 	<?php get_sidebar(); ?>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@ get_header();
 		<section class="error-content px-2 py-8 px-md-4 py-15 px-lg-6">
 			<h1 class="error-content__title mb-3 mb-lg-4"><?php echo esc_html_x( 'Das hat nicht funktioniert', '501 Page Title', 'nostrasponte' ); ?></h1>
 			<p class="error-content__text mb-6 mb-lg-12"><?php echo esc_html_x( 'Die von Dir gesuchte Seite kann nicht angezeigt werden…', '501 Page Text', 'nostrasponte' ); ?></p>
-			<p class="center"><a href="javascript:window.history.back()" class="btn center"><i class="feather icon-arrow-left"></i> <?php esc_html_e( 'Zurück', 'nostrasponte' ) ?></a></p>
+			<p class="center"><a href="javascript:window.history.back()" class="btn center"><i class="feather icon-arrow-left ml-minus"></i> <?php esc_html_e( 'Zurück', 'nostrasponte' ) ?></a></p>
 		</section>
 	</main>
     <?php get_sidebar(); ?>
