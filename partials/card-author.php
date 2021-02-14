8898c81ac23120a9e3aa98963c8298a4567049c8
Mike Kühnapfel <mike.kuehnapfel@smail.th-koeln.de>
2021-02-14 23:30:25 +0100

    add post template (also close #17)

---
+++ partials/card-author.php
@@ -0,0 +1,15 @@
+<?php $avatarArgs = [
+	'size'          => 96,
+	'default'       => 'mm'
+]; ?>
+
+<article class="card card--author">
+	<div style="background-image: url(<?php echo esc_attr(get_avatar_url(get_the_author_meta('ID'), $avatarArgs)); ?>);" title="<?php echo esc_attr(get_the_author_meta('display_name')); ?>" class="card--author__avatar" role="img"></div>
+	<div class="card--author__wrapper">
+		<h2 class="card--author__title my-0"><?php esc_html_e('Über den Autor', 'nostrasponte'); ?></h2>
+		<p class="card--author__description"><?php echo esc_html(get_the_author_meta('description')); ?></p>
+        <div class="card--author__email-wrapper fl-center mt-2">
+            <a href="mailto:<?php echo esc_attr(get_the_author_meta('user_email')); ?>" class="card--author__email btn btn--secondary"><?php echo esc_html(get_the_author_meta('user_email')); ?></a>
+        </div>
+	</div>
+</article>
--- partials/card-post.php
+++ partials/card-post.php
@@ -5,42 +5,44 @@
 	</header>
 	<p class="card--post__excerpt"><?php echo wp_trim_words(get_the_excerpt(), 18, '&hellip;'); ?></p>
 	<footer class="card--post__foot">
-		<div class="card--post__foot__category">
-			<i class="feather icon-bookmark" title="<?php esc_attr_e( 'Kategorien', 'nostrasponte' ); ?>"></i>
+		<div class="card--post__foot__category" title="<?php esc_attr_e( 'Kategorien', 'nostrasponte' ); ?>">
+			<i class="feather icon-bookmark"></i>
 			<ul>
-				<?php wp_list_categories( [ 'title_li' => '' ] ); ?>
+				<?php foreach (get_categories() as $cat) { ?>
+                    <li><a href="<?php echo get_category_link($cat); ?>" title="<?php echo esc_attr(sprintf('%s %s%s', __('Kategorie', 'nostrasponte'), $cat->name, empty($cat->description) ? '' : "\n\n" . strip_tags($cat->description))); ?>"><?php echo esc_html($cat->name); ?></a></li>
+				<?php } ?>
 			</ul>
 		</div>
 		<?php if ( has_tag() ) { ?>
-			<div class="card--post__foot__tags">
-				<i class="feather icon-tag" title="<?php esc_attr_e( 'Tags', 'nostrasponte' ); ?>"></i>
+			<div class="card--post__foot__tags" title="<?php esc_attr_e( 'Themen', 'nostrasponte' ); ?>">
+				<i class="feather icon-tag"></i>
 				<ul>
 					<?php foreach ( get_tags() as $tag ) { ?>
-						<li><a href="<?php echo get_tag_link( $tag->term_id ); ?>" <?php if ( !empty($tag->description) ) { ?>title="<?php echo esc_attr($tag->description); ?>"<?php } ?>><?php echo esc_html( $tag->name ); ?></a></li>
+                        <li><a href="<?php echo get_tag_link( $tag->term_id ); ?>" title="<?php echo esc_attr(sprintf('%s %s%s', __('Thema', 'nostrasponte'), $tag->name, empty($tag->description) ? '' : "\n\n" . strip_tags($tag->description))); ?>"><?php echo esc_html( $tag->name ); ?></a></li>
 					<?php } ?>
 				</ul>
 			</div>
 		<?php }
 		if( has_term( '', 'municipality' ) ) { ?>
-			<div class="card--post__foot__municipalities">
-				<i class="feather icon-home" title="<?php esc_attr_e( 'Kommune', 'nostrasponte' ); ?>"></i>
+			<div class="card--post__foot__municipalities" title="<?php esc_attr_e( 'Kommunen', 'nostrasponte' ); ?>">
+				<i class="feather icon-home"></i>
 				<ul>
-					<?php foreach ( get_tags( [ 'taxonomy' => 'municipality' ] ) as $tag ) { ?>
-						<li><a href="<?php echo get_tag_link( $tag->term_id ); ?>" <?php if ( !empty($tag->description) ) { ?>title="<?php echo esc_attr($tag->description); ?>"<?php } ?>><?php echo esc_html( $tag->name ); ?></a></li>
+					<?php foreach ( get_the_terms( get_the_ID(), 'municipality' ) as $tag ) { ?>
+                        <li><a href="<?php echo get_tag_link( $tag->term_id ); ?>" title="<?php $meta = get_term_meta($tag->term_id, 'municipality_data', true); echo esc_attr(sprintf('%s%s', isset($meta['long_title']) ? $meta['long_title'] : __('Kommune', 'nostrasponte') . ' ' . $tag->name , empty($tag->description) ? '' : "\n\n" . strip_tags($tag->description))); ?>"><?php echo esc_html( $tag->name ); ?></a></li>
 					<?php } ?>
 				</ul>
 			</div>
 		<?php } ?>
-		<div class="card--post__foot__date">
-			<i class="feather icon-calendar" title="<?php esc_attr_e( 'Datum', 'nostrasponte' ); ?>"></i>
+		<div class="card--post__foot__date" title="<?php echo esc_attr(sprintf('%s %s. %s %s', __("Datum\nveröffentlicht am", 'nostrasponte'), get_the_date('j'), NS_MONTH[intval(get_the_date('n'))], get_the_date('Y'))); ?>">
+			<i class="feather icon-calendar"></i>
 			<time datetime="<?php the_date( 'Y-m-d' ); ?>"><?php printf('%s, %s. %s %s',
 						NS_DAY_OF_WEEK[ intval( get_the_date( 'w' ) ) ],
 						get_the_date( 'j' ),
 						NS_MONTH[ intval( get_the_date( 'n' ) ) ],
 						get_the_date( 'Y' ) ); ?></time>
 		</div>
-		<div class="card--post__foot__author">
-			<i class="feather icon-user" title="<?php esc_attr_e('Autor', 'nostrasponte' ); ?>"></i><span><?php the_author(); ?></span>
+		<div class="card--post__foot__author" title="<?php echo esc_attr(sprintf('%s: %s', __('Autor', 'nostrasponte'), get_the_author())); ?>">
+			<i class="feather icon-user"></i><span><?php the_author(); ?></span>
 		</div>
 	</footer>
 </article>
