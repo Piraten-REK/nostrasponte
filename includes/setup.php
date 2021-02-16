bb549937db38ad1f4eece5de2c52b5717c793a09
Mike Kühnapfel <mike.kuehnapfel@smail.th-koeln.de>
2021-02-16 20:56:40 +0100

    fix typo in setup.php

--- includes/setup.php
+++ includes/setup.php
@@ -2,7 +2,7 @@
 
 function ns_setup_theme () {
 	add_theme_support( 'post-thumbnails' );
-	add_theme_support( 'atomatic-feed-links' );
+	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'html5' );
 	add_theme_support( 'custom-logo' );
