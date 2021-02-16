b39246450201ec644814df7952aa787eed3ca4d9
Mike Kühnapfel <mike.kuehnapfel@smail.th-koeln.de>
2021-02-16 21:01:26 +0100

    fix #25

--- includes/front/js.php
+++ includes/front/js.php
@@ -1,5 +1,6 @@
 <?php
 
 function ns_defer_js ($tag, $handle, $src) {
+	if (is_admin()) return $tag;
 	return '<script src="' . esc_url($src) . '"' . ($handle === 'ns_main_js' ? 'type="module"' : '') . '></script>';
 }
